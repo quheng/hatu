6efe0223a8f5b19085fe36352f903a9bd61f5b17
madaquheng <quheng@madadata.com>
2017-03-04 21:00:05 +0800

    update variable

--- server/dbTest.js
+++ server/dbTest.js
@@ -1,11 +1,38 @@
 import pg from 'pg'
+import _ from 'lodash'
+
+if (_.isEmpty(process.env.PG_HOST)) {
+  console.log('can not find pg host in environment variable')
+  process.exit(-1)
+}
+
+if (_.isEmpty(process.env.PG_PORT)) {
+  console.log('can not find pg host in environment variable')
+  process.exit(-1)
+}
+
+if (_.isEmpty(process.env.PG_USER)) {
+  console.log('can not find pg user in environment variable')
+  process.exit(-1)
+}
+
+if (_.isEmpty(process.env.PG_PASSWORD)) {
+  console.log('can not find pg password in environment variable')
+  process.exit(-1)
+}
+
+if (_.isEmpty(process.env.PG_DATABASE)) {
+  console.log('can not find pg database in environment variable')
+  process.exit(-1)
+}
+
 
 const config = {
-  user: 'hatu_user',
-  database: 'hatu_db',
-  password: 'hatu_password',
-  host: 'pg',
-  port: '5432',
+  user: process.env.PG_USER,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
 }
