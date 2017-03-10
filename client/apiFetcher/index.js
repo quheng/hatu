95d23e39b451c48e01e01e2164156da03090c5fc
quheng <494862190@qq.com>
2017-03-10 16:36:34 +0800

    add api fetcher

---
+++ client/apiFetcher/index.js
@@ -0,0 +1,44 @@
+
+export default class ApiFetcher {
+  constructor(config) {
+    this.config = config
+  }
+
+  async logIn ({ username, password }) {
+    return await fetch(`${this.config.server}/users/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': URLENCODED_FORMDATA
+      },
+      body: qs.stringify({
+        username,
+        password
+      }),
+      credentials: 'include'
+    })
+  }
+
+  async signUp ({ username, password, confirm, email, phone }) {
+    return await fetch(`${this.config.server}/users/signup`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': URLENCODED_FORMDATA
+      },
+      body: qs.stringify({
+        username,
+        password,
+        confirm,
+        email,
+        phone
+      }),
+      credentials: 'include'
+    })
+  }
+
+  async logOut () {
+    return await fetch(`${this.config.server}/users/logout`, {
+      method: 'POST',
+      credentials: 'include'
+    })
+  }
+}
